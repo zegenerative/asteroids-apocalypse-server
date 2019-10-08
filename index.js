a79b201b905c7f74b4d3ce873467b8e9bce6c958
future-ruins <helliot885@gmail.com>
2019-10-08 11:54:13 +0200

    Add user/model in index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(corsMiddleware, userRouter, authRouter);
 const port = process.env.PORT || 4000;
 const db = require('./db');
 const Room = require('./Room/model');
+const User = require('./User/model');
 
 function onListen() {
   console.log(`Listening on :${port}`);
