7ff02b2818d5cb3859c946a8c39e7d5e3ba77310
future-ruins <helliot885@gmail.com>
2019-10-08 12:10:28 +0200

    Change DB_URL heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const databaseUrl =
-  process.env.DATABASE_URL ||
+  'postgres://wzwfkstvwmsivs:a8df2de51a1d5f46bf6c1c04d8a31e8ad1fab6fe89e8609859916f4d24c079e3@ec2-54-217-206-65.eu-west-1.compute.amazonaws.com:5432/dc6j020hjsb0m2' ||
   'postgres://postgres:password@localhost:5432/postgres';
 
 console.log('database_url:', process.env.DATABASE_URL);
